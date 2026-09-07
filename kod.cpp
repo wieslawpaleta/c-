7826c392ea8d29341791c89b80bd7c5a2f342b67
wieslawpaleta <tankluk@gmail.com>
2026-09-07 18:20:59 +0200

    nauka

---
+++ kod.cpp
@@ -0,0 +1,40 @@
+//C++ w3schools
+//Introduction
+//Get Started with C++
+//Syntax
+//Statements
+//Syntax Code Challenge
+//Output (Print Text)
+//Print Numbers
+//New Lines
+
+
+#include <iostream>
+using namespace std;
+
+
+// int main() {
+//     std::cout << "Wer seid ihr?" << std::endl;
+//     return 0;
+// }
+
+
+int main() {
+    cout << "Hallo, Welt! Wie geht es euch?" << endl;
+    cout << "Wer seid ihr?" << endl;
+    cout << "\nHallo, Welt! Wie geht es euch? ";
+    cout << "Wer seid ihr?\n";
+    cout << 3 << endl;
+    cout << 5 << endl;
+    cout << 3 + 5 << endl;
+    cout << 3 * 5 << endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
