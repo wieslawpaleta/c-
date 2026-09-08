f33406fb55e65e1e73e2fd3339a1d0dc6ef4eaf8
wieslawpaleta <tankluk@gmail.com>
2026-09-08 21:31:48 +0200

    Add files via upload

--- kod.cpp
+++ kod.cpp
@@ -1,12 +1,13 @@
 //C++ w3schools
-//Introduction
-//Get Started with C++
-//Syntax
-//Statements
-//Syntax Code Challenge
-//Output (Print Text)
-//Print Numbers
-//New Lines
+//Introduction, Get Started with C++, Syntax, Statements, Syntax Code Challenge, Output (Print Text), Print Numbers, New Lines, Output Code Challenge, Comments
+/*Ja, ich weiß, wie man mehrzeilige Kommentare verwendet.*/
+//Variables(Tu jestem teraz), 
+//
+//
+//
+//
+//
+//
 
 
 #include <iostream>
@@ -28,9 +29,19 @@ int main() {
     cout << 5 << endl;
     cout << 3 + 5 << endl;
     cout << 3 * 5 << endl;
-
-
+    cout << "GrundLegendes" << "\n";
+    cout << "Dieser Kurs richtet sich an Anfänger\t";
+    cout << "Du kennst die Regeln" << endl;
+    int myNum = 15;
+    cout << myNum << endl;
+    double myDouble;
+    myDouble = 6.98;
+    cout << myDouble << endl;
+    char myChar = 'P';
+    myChar = 'Z';
+    cout << myChar << endl;
     return 0;
+
 }
 
 
