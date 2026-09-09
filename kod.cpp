bc6752a85e0796208322e20632abb0d3e8150c88
wieslawpaleta <tankluk@gmail.com>
2026-09-09 20:28:30 +0200

    Add files via upload

--- kod.cpp
+++ kod.cpp
@@ -1,7 +1,7 @@
 //C++ w3schools
 //Introduction, Get Started with C++, Syntax, Statements, Syntax Code Challenge, Output (Print Text), Print Numbers, New Lines, Output Code Challenge, Comments
 /*Ja, ich weiß, wie man mehrzeilige Kommentare verwendet.*/
-//Variables(Tu jestem teraz), 
+//VariablesDeclare, Multiple Variables, Identifiers, Constants, Variables Examples (Tu jestem teraz)
 //
 //
 //
@@ -32,14 +32,31 @@ int main() {
     cout << "GrundLegendes" << "\n";
     cout << "Dieser Kurs richtet sich an Anfänger\t";
     cout << "Du kennst die Regeln" << endl;
-    int myNum = 15;
-    cout << myNum << endl;
-    double myDouble;
-    myDouble = 6.98;
-    cout << myDouble << endl;
-    char myChar = 'P';
-    myChar = 'Z';
-    cout << myChar << endl;
+    int meinNum = 15;
+    cout << meinNum << endl;
+    double meinDouble;
+    meinDouble = 6.98;
+    cout << meinDouble << endl;
+    char meinChar = 'P';
+    meinChar = 'Z';
+    cout << meinChar << endl;
+    string meinText = "der Begriff";
+    bool meinBooLean = true;
+    cout << meinText << " Nummer " << meinBooLean << endl;
+    double sum = meinNum + meinDouble;
+    cout << sum << endl;
+    int x = 9, y = 3, z = 2;
+    cout << x + y + z << endl;
+    int a, b, c;
+    a = b = c = 50;
+    cout << a + b + c << endl;
+    string pilzArt = "der Fliegenpilz";
+    cout << pilzArt << endl;
+    const int kilometerProStunde = 60;
+    cout << kilometerProStunde << endl;
+    // kilometerProStunde = 30; //error
+    // cout << kilometerProStunde << endl;
+
     return 0;
 
 }
